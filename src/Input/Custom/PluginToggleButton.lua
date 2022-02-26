dba257eb095aba993223602a7ceb42dcb59f6e9b
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2022-02-26 00:12:27 -0500

    Add plugin toggle button.

---
+++ src/Input/Custom/PluginToggleButton.lua
@@ -0,0 +1,45 @@
+--[[
+TheNexusAvenger
+
+Toggle button for a PluginGui.
+--]]
+
+local NexusPluginComponents = require(script.Parent.Parent.Parent)
+
+local PluginInstance = NexusPluginComponents:GetResource("Base.PluginInstance")
+
+local PluginToggleButton = PluginInstance:Extend()
+PluginToggleButton:SetClassName("PluginToggleButton")
+
+
+
+--[[
+Creates the Plugin Toggle Button.
+--]]
+function PluginToggleButton:__new(Button, PluginGui)
+	self:InitializeSuper(Button)
+
+	--Set up the changed event.
+	PluginGui:GetPropertyChangedSignal("Enabled"):Connect(function()
+        self:SetActive(PluginGui.Enabled)
+	end)
+
+	--Set up toggling the PluginGui.
+	local DB = true
+	self.Click:Connect(function()
+		if DB then
+			DB = false
+			PluginGui.Enabled = not PluginGui.Enabled
+            self:SetActive(PluginGui.Enabled)
+			wait()
+			DB = true
+		end
+	end)
+
+	--Set the default.
+    self:SetActive(PluginGui.Enabled)
+end
+
+
+
+return PluginToggleButton
