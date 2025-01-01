3071717f39a27b44e637f16972ab14e643c2dada
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2024-12-31 19:01:19 -0500

    Remove obsolete classes.

--- src/init.lua
+++ src/init.lua
@@ -8,15 +8,8 @@ Main project for using Nexus Plugin Components.
 local NexusEvent = require(script:WaitForChild("NexusInstance"):WaitForChild("Event"):WaitForChild("NexusEvent"))
 local PluginColor = require(script:WaitForChild("Base"):WaitForChild("PluginColor"))
 local PluginInstance = require(script:WaitForChild("Base"):WaitForChild("PluginInstance"))
-local Checkbox = require(script:WaitForChild("Input"):WaitForChild("Custom"):WaitForChild("Checkbox"))
 local CollapsableListFrame = require(script:WaitForChild("Input"):WaitForChild("Custom"):WaitForChild("CollapsableListFrame"))
 local ElementList = require(script:WaitForChild("Input"):WaitForChild("Custom"):WaitForChild("ElementList"))
-local PluginToggleButton = require(script:WaitForChild("Input"):WaitForChild("Custom"):WaitForChild("PluginToggleButton"))
-local Slider = require(script:WaitForChild("Input"):WaitForChild("Custom"):WaitForChild("Slider"))
-local ImageButton = require(script:WaitForChild("Input"):WaitForChild("Roblox"):WaitForChild("ImageButton"))
-local TextButton = require(script:WaitForChild("Input"):WaitForChild("Roblox"):WaitForChild("TextButton"))
-local TextBox = require(script:WaitForChild("Input"):WaitForChild("Roblox"):WaitForChild("TextBox"))
-local UserInputService = require(script:WaitForChild("Input"):WaitForChild("Service"):WaitForChild("UserInputService"))
 local SelectionList = require(script:WaitForChild("State"):WaitForChild("SelectionList"))
 
 local NexusPluginComponents = {}
@@ -29,30 +22,15 @@ NexusPluginComponents.ClassNameToPath = {
     ElementList = ElementList,
     SelectionList = SelectionList,
 
-    --Roblox.
-    ImageButton = ImageButton,
-    TextButton = TextButton,
-    TextBox = TextBox,
-
     --Custom.
-    Checkbox = Checkbox,
     CollapsableListFrame = CollapsableListFrame,
-    PluginToggleButton = PluginToggleButton,
-    Slider = Slider,
 }
 
 export type Event<T...> = NexusEvent.NexusEvent<T...>
 export type PluginColor = PluginColor.PluginColor
 export type PluginInstance = PluginInstance.PluginInstance
-export type Checkbox = Checkbox.Checkbox
 export type CollapsableListFrame = CollapsableListFrame.CollapsableListFrame
 export type ElementList = ElementList.ElementList
-export type PluginToggleButton = PluginToggleButton.PluginToggleButton
-export type Slider = Slider.Slider
-export type ImageButton = ImageButton.PluginImageButton
-export type TextButton = TextButton.PluginTextButton
-export type TextBox = TextBox.PluginTextBox
-export type UserInputService = UserInputService.PluginUserInputService
 export type SelectionList = SelectionList.SelectionList
 
 
