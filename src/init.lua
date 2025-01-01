6dfc6302449fc20e863580e48c0834a008ec78b5
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2024-12-31 23:27:33 -0500

    Remove Nexus Wrapped Instance.

--- src/init.lua
+++ src/init.lua
@@ -5,24 +5,14 @@ Main project for using Nexus Plugin Components.
 --]]
 --!strict
 
-local NexusEvent = require(script:WaitForChild("NexusInstance"):WaitForChild("Event"):WaitForChild("NexusEvent"))
-local PluginColor = require(script:WaitForChild("Base"):WaitForChild("PluginColor"))
-local PluginInstance = require(script:WaitForChild("Base"):WaitForChild("PluginInstance"))
 local SelectionList = require(script:WaitForChild("State"):WaitForChild("SelectionList"))
 
 local NexusPluginComponents = {}
 NexusPluginComponents.ClassNameToPath = {
-    --Nexus Instance.
-    Event = NexusEvent,
-
     --Helper.
-    PluginColor = PluginColor,
     SelectionList = SelectionList,
 }
 
-export type Event<T...> = NexusEvent.NexusEvent<T...>
-export type PluginColor = PluginColor.PluginColor
-export type PluginInstance = PluginInstance.PluginInstance
 export type SelectionList = SelectionList.SelectionList
 
 
@@ -34,7 +24,7 @@ function NexusPluginComponents.new(ClassName: string, ...: any): any
     if NexusPluginComponents.ClassNameToPath[ClassName] then
         return NexusPluginComponents.ClassNameToPath[ClassName].new(...)
     end
-    return PluginInstance.new(ClassName)
+    return nil
 end
 
 --[[
